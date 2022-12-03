870b3b9271348b00754edd51eda6aa839e378e2b
GustavoEmanuel1995 <gustavo38893730@gmail.com>
2022-12-03 18:16:43 -0300

    Primero
    Primero

---
+++ AGENDA-TELEFONICA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AGENDA_TELEFONICA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
